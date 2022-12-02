316bfce5111e2fc41659f3f77b3e56fcc01c9fe3
yshaikh165 <yshaikh165@hotmail.com>
2022-12-02 12:40:50 -0600

    was working on unit test

--- frontend/src/__tests__/Login.test.js
+++ frontend/src/__tests__/Login.test.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client";
 import Login from "../components/LoginForm";
-
 
 it("renders without crashing", ()=>{
     const div = document.createElement("div");
     ReactDOM.render(<Login></Login>, div)
 })
+
+
+
+
 
 
--- frontend/src/__tests__/api.test.js
+++ frontend/src/__tests__/api.test.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 jest.mock('axios')
 import { reservationsGetRequest } from '../components/SearchReservation.js'
-import { tableGetRequest } from '../components/TablesAvailable.js'
+//import { tableGetRequest } from '../components/TablesAvailable.js'
 import { reservationsEndpoint, tableEndpoint } from '../constantValues.js'
 //import * as apiservice from '../components/TablesAvailable'
 
